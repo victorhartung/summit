c4a8df19d570754216669c082d4e5b7ca105923d
Victor Hugo Varelo Hartung <victorhartung@ciandt.com>
2024-09-04 19:39:16 -0300

    teste

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-zinc-400 font-regular text-center text-lg mt-10">
-        React Native
+        react
       </Text>
     </View>
   );
