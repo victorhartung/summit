413301f837b80b3f01b2cf4b144aba4ec9f5b4e6
Pablo Irineu de Souza <pablo.souza.5@outlook.com.br>
2024-09-04 00:38:50 -0300

    Add fonts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text>React Native - PM II</Text>
+      <Text className="text-zinc-400">React Native</Text>
     </View>
   );
 }
