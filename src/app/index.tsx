02d1746bca4a75de66714e6ef41d5e5ac1b590c8
Pablo Irineu de Souza <pablo.souza.5@outlook.com.br>
2024-09-04 00:18:21 -0300

    Config project

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>React Native - PM II</Text>
+    </View>
+  );
+}
