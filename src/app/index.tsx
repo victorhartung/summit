0e9238e04990252cb670ff7b01fb376f34367722
Pablo Irineu de Souza <pablo.souza.5@outlook.com.br>
2024-09-04 00:44:57 -0300

    Use font

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,9 @@ import { View, Text } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-zinc-400">React Native</Text>
+      <Text className="text-zinc-400 font-regular text-center text-lg mt-10">
+        React Native
+      </Text>
     </View>
   );
 }
